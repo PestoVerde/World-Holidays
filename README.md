16d1c5aa56cb7887b521d4bce3498f19319ad084
Sergey Cherkasov <remoteproduction@yandex.ru>
2016-02-19 16:46:09 +0300

    Interface finished

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # World-Holidays
+
+Before run code please:
+1. upload the data with SQL quiery at https://modeanalytics.com/reference_lookups/tables/holidays_by_country
+2. Put it to working directory
+3. Run cleaning_data.R
--- ui.R
+++ ui.R
@@ -2,7 +2,25 @@ library(shiny)
 library(plotly)
 
 shinyUI(navbarPage(theme = "bootstrap.min.css", "World Holidays",
-                   tabPanel("Map"),
+                   tabPanel("Map",
+                            sidebarLayout(
+                                
+                                sidebarPanel(
+                                    radioButtons("radio", 
+                                                 label = h3("Radio button"),
+                                                 choices = list("All Indian holidays" = 1,
+                                                                "National Indian holidays" = 2,
+                                                                "Christmas" = 3,
+                                                                "Prophet's birthday" = 4,
+                                                                "Both Christmas and Prophet's birthday" = 5),
+                                                 selected = 1)#radiButtons
+                                ),#sidebarPanel
+                                
+                                mainPanel(
+                                    plotOutput("distPlot")
+                                )#mainPanel
+                            )#sidebarLayout
+                            ),#tabPanel(Map)
                    tabPanel("Documentation",
                             p("India is multiconfessional country and respects
                               all the confessions. That is why there are 77
@@ -14,9 +32,12 @@ shinyUI(navbarPage(theme = "bootstrap.min.css", "World Holidays",
                               Christmas or Prophet's Birthday or both."),
                             p("When you hover your cursor over the map, you
                               can see information about countries' holidays."),
-                            p("Code can be found at ")
+                            p("Code can be found at", a("GitHub",
+                            href="https://github.com/PestoVerde/World-Holidays"))
                             ),
                    tabPanel("Credits",
-                            p("Thank team https://about.modeanalytics.com
-                              and especially Benn for idea, data and help."))
+                            p("Thank team of", 
+                              a("ModelAnalytic", 
+                                href="https://about.modeanalytics.com"),
+                              "for idea and data. Special thanks Benn for help."))
 ))
